47558221f042f9fd947ff33ef4cc553c96a667b8
Nic West <nicwest@mailbox.org>
2024-08-23 15:39:23 +0100

    holder v1

---
+++ out/thing.scad
@@ -0,0 +1,48 @@
+color ([0.2, 0.2, 0.2, ]) {
+  translate ([0, 0, 3]) {
+    cube ([44, 44, 6], center=true);
+  }
+}
+color ([0.9, 0.9, 0.9, ]) {
+  translate ([0, 0, 15]) {
+    rotate (a=90.0, v=[0, 1, 0]) {
+      cube ([18, 18, 70], center=true);
+    }
+  }
+}
+color ([0.44, 0.84, 0.15, ]) {
+  translate ([0, 0, 15]) {
+    rotate (a=90.0, v=[0, 1, 0]) {
+      union () {
+        difference () {
+          cube ([8, 20.5, 72.5], center=true);
+          cube ([1000, 18.5, 70.5], center=true);
+        }
+        translate ([0, 13.25, 0]) {
+          rotate (a=90.0, v=[1, 0, 0]) {
+            union () {
+              translate ([0, -8, 0]) {
+                cube ([8, 6, 6], center=true);
+              }
+              translate ([0, 8, 0]) {
+                cube ([8, 6, 6], center=true);
+              }
+            }
+          }
+        }
+        translate ([0, -13.25, 0]) {
+          rotate (a=90.0, v=[1, 0, 0]) {
+            union () {
+              translate ([0, -8, 0]) {
+                cube ([8, 6, 6], center=true);
+              }
+              translate ([0, 8, 0]) {
+                cube ([8, 6, 6], center=true);
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}
--- src/cad/core.clj
+++ src/cad/core.clj
@@ -1,10 +1,19 @@
 (ns cad.core
   (:require [clojure.java.io :refer [make-parents]]
             [scad-clj.scad :refer [write-scad]]
-            [scad-clj.model :refer [cube]])
+            [scad-clj.model :refer [cube color translate rotate union difference]])
   (:gen-class))
 
 (defn degrees [n] (* n (/ Math/PI 180)))
+
+(def black
+  [0.2 0.2 0.2])
+
+(def silver
+  [0.9 0.9 0.9])
+
+(def green
+  [0.44 0.84 0.15])
 
 (defn render!
   [file-name part]
@@ -14,17 +23,97 @@
           (write-scad part))))
 
 (defn config
-  ([] (config {}))
+  ([] (config {:frame-thickness 6
+               :frame-width 44
+               :battery-width 18
+               :battery-height 18
+               :battery-length 70
+               :band-clearance 0.5
+               :band-thickness 2
+               :band-width 8
+               :progger-width 6
+               :progger-thickness 6
+               :velcro-width 10
+               }))
   ([opts]
-  (let [opts (merge {:pretty? false }
-                    opts)]
+   (let [opts (merge {:pretty? false }
+                     opts)]
 
-    opts)))
+     opts)))
 
-(def thing
-  (cube 10 10))
+(defn frame
+  [{:keys [frame-width frame-thickness]}]
+  (translate [0 0 (/ frame-thickness 2)]
+             (cube frame-width frame-width frame-thickness)))
 
-(render! "thing" thing)
+
+(defn battery
+  [{:keys [battery-width battery-height battery-length frame-thickness]}]
+  (translate [0 0 (+ (/ battery-height 2) frame-thickness)]
+             (rotate (degrees 90) [0 1 0]
+                     (cube battery-width battery-height battery-length))))
+
+(defn progger
+  [{:keys [progger-width band-width progger-thickness]}]
+  (cube band-width progger-width  progger-thickness))
+
+
+(defn progger-set
+  [{:keys [band-thickness progger-width progger-thickness velcro-width]
+    :as cfg}]
+  (let [d (+ (/ velcro-width 2) (/ progger-width 2))
+        b (- d)
+        z (/ progger-thickness 2)
+        z 0]
+    (union
+      (translate [0 b z]
+                 (progger cfg))
+      (translate [0 d z]
+                 (progger cfg))
+      )))
+
+(defn holder
+  [{:keys [battery-width battery-length band-thickness band-clearance band-width
+           frame-thickness battery-height progger-thickness]
+    :as cfg}]
+  (let [d (/ (+ battery-width band-thickness band-clearance progger-thickness) 2)]
+    (translate [0 0 (+ (/ band-width 2) (/ (- battery-height band-width) 2) frame-thickness)]
+                            (rotate (degrees 90) [0 1 0]
+                                      (union
+                                        (difference
+                                          (cube 
+                                            band-width
+                                            (+ battery-width band-thickness band-clearance)
+                                            (+ battery-length band-thickness band-clearance)
+                                            )
+                                          (cube
+                                            1000
+                                            (+ battery-width band-clearance)
+                                            (+ battery-length band-clearance)))
+                                        (translate [0 d 0]
+                                                   (rotate (degrees 90) [1 0 0]
+                                                           (progger-set cfg)))
+                                        (translate [0 (- d) 0]
+                                                   (rotate (degrees 90) [1 0 0]
+                                                           (progger-set cfg))
+                                                   ))))
+ ))
+
+(defn thing
+  [cfg]
+  [
+   (color black
+          (frame cfg))
+
+   (color silver
+          (battery cfg))
+
+   (color green
+          (holder cfg))])
+
+(render! "holder" (holder (config)))
+(render! "thing" (thing (config)))
+;(render! "thing" (progger-set (config)))
 
 
 (defn -main
